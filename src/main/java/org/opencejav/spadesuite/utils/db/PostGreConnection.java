ff83bdc7f57772bc01270d47c769d641ad1523a7
Muhammad Bilal Khan <lofi.audit@gmail.com>
2024-04-25 02:03:03 -0400

    Add: PostGreConnection.java Without Hikari Pooling

---
+++ src/main/java/org/opencejav/spadesuite/utils/db/PostGreConnection.java
@@ -0,0 +1,8 @@
+package org.opencejav.spadesuite.utils.db;
+
+import org.opencejav.spadesuite.annotations.UtilityClass;
+
+@UtilityClass(className = "PostGreConnection")
+public class PostGreConnection {
+    // TODO Implement PostGreConnection Class (Without Hikari)
+}
